54fd0c467a4c5198ef7720113d66c71a06f19cb2
Robin Jose Mejia Mancebo <robinjosemejiamancebo@gmail.com>
2019-11-20 22:24:28 -0400

    Modified

---
+++ Ubetarepo/Ubeta/Views/Signuppage.xaml.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace Ubeta.Views
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class Signuppage : ContentPage
+    {
+        public Signuppage()
+        {
+            InitializeComponent();
+        }
+    }
+}
